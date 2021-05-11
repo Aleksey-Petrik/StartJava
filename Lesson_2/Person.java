6d8e01fe0ad21e196ee1051f82aaf39aca9512f0
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-11 19:18:37 +1000

    first obj

---
+++ Lesson_2/Person.java
@@ -0,0 +1,31 @@
+public class Person {
+    char sex = 'M';
+    String name = "Jon Jonson";
+    float height = 1.75f, weight = 75f;
+    int age = 34;
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.name);
+    }
+
+    public void walk() {
+
+    }
+
+    public void sit() {
+
+    }
+
+    public void run() {
+
+    }
+
+    public void talk() {
+
+    }
+
+    public void learnJava() {
+
+    }
+}
