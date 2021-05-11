a794fe3fe6f3d098b0028a623a1f87a59098556c
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-11 21:36:25 +1000

    class Wolf

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    char sex;
+    String nickName;
+    float weight; 
+    int age; 
+    String color;
+
+    public void walk() {
+        System.out.println("Ходит");
+    }
+
+    public void sit() {
+        System.out.println("Сидит");
+    }
+
+    public void run() {
+        System.out.println("Бежит");
+    }
+
+    public void howl() {
+        System.out.println("Воет");
+    }
+
+    public void hunt() {
+        System.out.println("Охотится");
+    }
+}
