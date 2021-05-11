7e211200f905a706ad5388bed6052ef4b518a85c
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-11 17:43:15 +1000

    code design

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,29 +1,32 @@
-public class ConditionalStatement{
-    public static void main(String[] args){
+public class ConditionalStatement {
+    public static void main(String[] args) {
         int age = 34;
-        float height = 1.75f;
-        boolean isMale = true;
-        char firstChar = 'A';
         if(age > 20) {
             System.out.println("Вам больше 20.");
         }
+
+        boolean isMale = true;
         if(isMale) {
             System.out.println("Вы мужчина.");
         }
         if(!isMale) {
             System.out.println("Вы женщина.");
         }
+
+        float height = 1.75f;
         if(height < 1.80) {
             System.out.println("Вы ниже 1.80.");
         } else {
             System.out.println("Вы выше 1.80.");
         }
-        if(firstChar == 'M') {
+
+        char firstLetterName = 'A';
+        if(firstLetterName == 'M') {
             System.out.println("Ваше имя начинается с буквы - " + 'M');
-        } else if(firstChar == 'I') {
+        } else if(firstLetterName == 'I') {
             System.out.println("Ваше имя начинается с буквы - " + 'I');
         } else {
-            System.out.println("Ваше имя начинается с буквы - " + firstChar);
+            System.out.println("Ваше имя начинается с буквы - " + firstLetterName);
         } 
     }
 }
