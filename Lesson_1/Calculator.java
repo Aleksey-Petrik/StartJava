a8578148cef0978c44a720fd916759cd4b12e423
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-11 17:43:02 +1000

    code design

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,12 +1,13 @@
-public class Calculator{
-    public static void main(String[] args){
+public class Calculator {
+    public static void main(String[] args) {
         int a = 9, b = 3;
-        double result = 0d;
+        double result = 0;
         char action = '/';
+
         if (action == '+') result = a + b; 
         if (action == '-') result = a - b;
         if (action == '*') result = a * b;
-        if (action == '/') result = (double)(a) / b;
+        if (action == '/') result = (double) a / b;
         if (action == '%') result = a % b;
         if (action == '^') {
             result = a;
