f6de4d3727782404933d63b1b1c71a935c220631
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-12 15:31:44 +1000

    new Calculator

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public double calc(int a, int b, char action) {
+        double result = 0;
+        switch(action) {
+            case('+'): result = a + b; break;
+            case('-'): result = a - b; break;
+            case('*'): result = a * b; break;
+            case('/'): result = (double) a / b; break;
+            case('%'): result = a % b; break;
+            case('^'): {
+                result = a;
+                for (int i = 1; i < b; i++) result *= a; 
+                break;
+            }
+        }
+        return result;
+    }
+}
