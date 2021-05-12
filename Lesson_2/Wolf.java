1ef0b66d8c531a0fbd3236c9e4e2f3369b09bf5b
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-12 14:27:11 +1000

    code design

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,26 +1,59 @@
 public class Wolf {
-    char sex;
-    String nickName;
-    float weight; 
-    int age; 
-    String color;
+    private char sex;
+    private String nickName;
+    private float weight; 
+    private int age; 
+    private String color;
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+        this.age = age;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+    public String getNickName() {
+        return nickName;
+    }
+    public float getWeight() {
+        return weight;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getColor() {
+        return color;
+    }
 
     public void walk() {
         System.out.println("Ходит");
     }
-
     public void sit() {
         System.out.println("Сидит");
     }
-
     public void run() {
         System.out.println("Бежит");
     }
-
     public void howl() {
         System.out.println("Воет");
     }
-
     public void hunt() {
         System.out.println("Охотится");
     }
