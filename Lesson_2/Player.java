0a73154211af3c6a19789c8e15832cf891b448f9
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-12 18:33:07 +1000

    Player

---
+++ Lesson_2/Player.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int inNumber(Scanner scanner) {
+        return scanner.nextInt();
+    }
+}
