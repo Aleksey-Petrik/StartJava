905e6681e8db0bd4accfb2de460f472481b05521
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-12 14:27:22 +1000

    code design

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,17 +2,17 @@ public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
 
-        wolf.sex = 'M';
-        wolf.nickName = "Alha";
-        wolf.weight = 120;
-        wolf.age = 34;
-        wolf.color = "black";
+        wolf.setSex('M');
+        wolf.setNickName("Alha");
+        wolf.setWeight(120);
+        wolf.setAge(7);
+        wolf.setColor("black");
 
-        System.out.println("Кличка - " + wolf.nickName);
-        System.out.println("Пол - " + wolf.sex);
-        System.out.println("Вес - " + wolf.weight);
-        System.out.println("Возраст - " + wolf.age);
-        System.out.println("Цвет - " + wolf.color);
+        System.out.println("Кличка - " + wolf.getNickName());
+        System.out.println("Пол - " + wolf.getSex());
+        System.out.println("Вес - " + wolf.getWeight());
+        System.out.println("Возраст - " + wolf.getAge());
+        System.out.println("Цвет - " + wolf.getColor());
 
         wolf.walk();
         wolf.hunt();
