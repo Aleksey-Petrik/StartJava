bf89780926381d59811e79ab66820baab9894671
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-18 17:15:30 +1000

    load data

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,12 @@
+INSERT INTO jaegers (modelName, mark, height, weight, status, origin, launch, kaijuKill) 
+	VALUES
+		('Gipsy Danger',     'Mark-3', '260',    '1980', 'Destroyed', 'United States of America', '20170710', '6'),
+		('Crimson Typhoon',  'Mark-4', '230',    '2080', 'Worker',    'China',                    '20150515', '13'),
+		('Brawler Yukon',    'Mark-1', '290',    '2310', 'Worker',    'South Korea',              '20130114', '5'),
+		('Horizon Brave',    'Mark-1', '310',    '1680', 'Destroyed', 'Mexico',                   '20150415', '7'),
+		('Diablo Intercept', 'Mark-2', '190',    '2480', 'Worker',    'United States of America', '20131223', '14'),
+		('Puma Real',        'Mark-2', '267.70', '1789', 'Destroyed', 'China',                    '20181026', '2'),
+		('Matador Fury',     'Mark-3', '268',    '1866', 'Destroyed', 'South Korea',              '20160807', '11'),
+		('Vulcan Specter',   'Mark-3', '250',    '1856', 'Worker',    'Mexico',                   '20190101', '9'),
+		('Striker Eureka',   'Mark-5', '230.90', '2630', 'Worker',    'United States of America', '20140319', '10'),
+		('Mammoth Apostle',  'Mark-4', '216',    '3050', 'Worker',    'South Korea',              '20160516', '15');
