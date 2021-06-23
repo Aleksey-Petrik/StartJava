d57899c9347401a1b6e73e07c1cc955583de5f44
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-23 11:48:43 +1000

    new sql query

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,6 +1,11 @@
 CREATE DATABASE Jaegers;
 --\c jaegers; --коннект к базе 
-CREATE TABLE jaegers (id SERIAL PRIMARY KEY,
-                      modelName TEXT, mark CHAR(7),
-                      height REAL, weight REAL, status CHAR(9),
-                      origin TEXT, launch DATE, kaijuKill INTEGER);
+CREATE TABLE jaegers (id        SERIAL PRIMARY KEY,
+                      modelName TEXT, 
+					  mark      CHAR(7),
+                      height    REAL, 
+					  weight    REAL, 
+					  status    CHAR(9),
+                      origin    TEXT, 
+					  launch    DATE, 
+					  kaijuKill INTEGER);
